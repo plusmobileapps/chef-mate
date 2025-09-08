2e9feef4529fc995280d4c82e1eea75ad123b228
Andrew Steinmetz <andrew@plusmobileapps.com>
2025-09-08 14:18:55 -0700

    move database dependencies into its own component within the database module

--- client/composeApp/src/androidMain/kotlin/com/plusmobileapps/chefmate/di/AndroidApplicationComponent.kt
+++ client/composeApp/src/androidMain/kotlin/com/plusmobileapps/chefmate/di/AndroidApplicationComponent.kt
@@ -3,8 +3,6 @@ package com.plusmobileapps.chefmate.di
 import android.content.Context
 import com.plusmobileapps.chefmate.ApplicationComponent
 import com.plusmobileapps.chefmate.client.database.DriverFactory
-import com.plusmobileapps.chefmate.database.Database
-import com.plusmobileapps.chefmate.database.GroceryQueries
 import me.tatarka.inject.annotations.Provides
 import software.amazon.lastmile.kotlin.inject.anvil.AppScope
 import software.amazon.lastmile.kotlin.inject.anvil.MergeComponent
@@ -18,14 +16,4 @@ abstract class AndroidApplicationComponent(
 
     @Provides
     fun driverFactory(): DriverFactory = DriverFactory(context = context)
-
-    @SingleIn(AppScope::class)
-    @Provides
-    fun database(driverFactory: DriverFactory): Database =
-        Database.Companion.invoke(driverFactory.createDriver())
-
-    @SingleIn(AppScope::class)
-    @Provides
-    fun providesGroceryQueries(database: Database): GroceryQueries = database.groceryQueries
-
 }
