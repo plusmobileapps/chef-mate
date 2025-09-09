dc24dae1725b1df88de26a00eecc2241a183c660
Andrew Steinmetz <andrew@plusmobileapps.com>
2025-09-08 22:22:38 -0700

    try to fix ios version

--- client/composeApp/src/iosMain/kotlin/com/plusmobileapps/chefmate/RootBloc.kt
+++ client/composeApp/src/iosMain/kotlin/com/plusmobileapps/chefmate/RootBloc.kt
@@ -1,13 +1,17 @@
 package com.plusmobileapps.chefmate
 
 import com.arkivanov.decompose.ComponentContext
+import com.plusmobileapps.chefmate.di.IosApplicationComponent
+import com.plusmobileapps.chefmate.di.createIosAppComponent
 import com.plusmobileapps.chefmate.root.RootBloc
 import com.plusmobileapps.chefmate.root.RootBlocFactory
+import platform.UIKit.UIApplication
 
 fun buildRootBloc(
     componentContext: ComponentContext,
+    application: UIApplication,
 ): RootBloc {
-    val applicationComponent = IosApplicationComponent()
+    val applicationComponent = IosApplicationComponent::class.createIosAppComponent(application)
     return RootBlocFactory.create(
         context = DefaultBlocContext(
             componentContext = componentContext,
---
+++ client/composeApp/src/iosMain/kotlin/com/plusmobileapps/chefmate/di/IosApplicationComponent.kt
@@ -0,0 +1,27 @@
+package com.plusmobileapps.chefmate.di
+
+import com.plusmobileapps.chefmate.ApplicationComponent
+import com.plusmobileapps.chefmate.client.database.DriverFactory
+import me.tatarka.inject.annotations.Provides
+import platform.UIKit.UIApplication
+import software.amazon.lastmile.kotlin.inject.anvil.AppScope
+import software.amazon.lastmile.kotlin.inject.anvil.MergeComponent
+import software.amazon.lastmile.kotlin.inject.anvil.SingleIn
+import kotlin.reflect.KClass
+
+@SingleIn(AppScope::class)
+@MergeComponent(AppScope::class)
+abstract class IosApplicationComponent(
+    @get:Provides val application: UIApplication,
+) : ApplicationComponent {
+
+    @Provides
+    fun driverFactory(): DriverFactory = DriverFactory()
+}
+
+/**
+ * The `actual fun` will be generated for each iOS specific target. See [MergeComponent] for
+ * more details.
+ */
+@MergeComponent.CreateComponent
+expect fun KClass<IosApplicationComponent>.createIosAppComponent(application: UIApplication): IosApplicationComponent
