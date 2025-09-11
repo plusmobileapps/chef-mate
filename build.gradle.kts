a457222509bf3336acb152f9db9c1711cf46b90c
Andrew Steinmetz <andrew@plusmobileapps.com>
2025-09-11 01:51:53 -0700

    add testing helpers with mokkery, kotest, and coroutines test

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,4 +11,6 @@ plugins {
     alias(libs.plugins.kotlinSerialization) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.ktlint) apply false
+    alias(libs.plugins.mokkery) apply false
+    alias(libs.plugins.kotest) apply false
 }
