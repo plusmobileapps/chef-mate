e59e8a8da89825c0a2f1e9122f38a416aee8b6d1
Andrew Steinmetz <andrew@plusmobileapps.com>
2025-09-13 00:02:16 -0700

    implement root bloc tests

--- client/root/impl/src/commonTest/kotlin/com/plusmobileapps/chefmate/root/RootBlocTest.kt
+++ client/root/impl/src/commonTest/kotlin/com/plusmobileapps/chefmate/root/RootBlocTest.kt
@@ -14,8 +14,8 @@ import io.kotest.matchers.types.instanceOf
 
 class RootBlocTest : FunSpec({
     val context = TestBlocContext.create()
-    var listOutput: Consumer<GroceryListBloc.Output>? = null
-    var detailOutput: Consumer<GroceryDetailBloc.Output>? = null
+    var listOutput: Consumer<GroceryListBloc.Output> = Consumer {}
+    var detailOutput: Consumer<GroceryDetailBloc.Output> = Consumer {}
     var detailId: Long? = null
 
     val rootBloc = RootBlocImpl(
@@ -34,19 +34,24 @@ class RootBlocTest : FunSpec({
     fun RootBloc.instance(): RootBloc.Child =
         state.value.active.instance
 
-    suspendTest("fail this test") {
-         1 shouldBe 1
-    }
-
-    suspendTest("When initialize, Then initial state is the grocery list") {
+    suspendTest("When initialized, Then list is shown") {
         rootBloc.instance() should instanceOf<RootBloc.Child.GroceryList>()
+        rootBloc.state.value.backStack.size shouldBe 0
     }
 
     suspendTest("Given list, When list outputs open detail, Then detail is shown") {
         val itemId = 123L
-        listOutput!!.onNext(GroceryListBloc.Output.OpenDetail(itemId))
+        listOutput.onNext(GroceryListBloc.Output.OpenDetail(itemId))
         rootBloc.instance() should instanceOf<RootBloc.Child.GroceryDetail>()
+        rootBloc.state.value.backStack.size shouldBe 1
         detailId shouldBe itemId
+    }
+
+    suspendTest("Given detail, When detail outputs close, Then list is shown") {
+        listOutput.onNext(GroceryListBloc.Output.OpenDetail(123L))
+        rootBloc.instance() should instanceOf<RootBloc.Child.GroceryDetail>()
+        detailOutput.onNext(GroceryDetailBloc.Output.Finished)
+        rootBloc.instance() should instanceOf<RootBloc.Child.GroceryList>()
     }
 
     this.isolationMode = IsolationMode.InstancePerRoot
