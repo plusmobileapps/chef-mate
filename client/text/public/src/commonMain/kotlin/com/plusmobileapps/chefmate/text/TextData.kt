d3a85a223e2ffb39a2ca393f0d119221aabff5c6
Andrew Steinmetz <andrew@plusmobileapps.com>
2025-11-03 00:24:55 -0800

    add plural resource string

--- client/text/public/src/commonMain/kotlin/com/plusmobileapps/chefmate/text/TextData.kt
+++ client/text/public/src/commonMain/kotlin/com/plusmobileapps/chefmate/text/TextData.kt
@@ -1,8 +1,13 @@
 package com.plusmobileapps.chefmate.text
 
 import androidx.compose.runtime.Composable
+import com.plusmobileapps.chefmate.text.PhraseModel
+import org.jetbrains.compose.resources.PluralStringResource
 import org.jetbrains.compose.resources.StringResource
+import org.jetbrains.compose.resources.pluralStringResource
 import org.jetbrains.compose.resources.stringResource
+import kotlin.collections.component1
+import kotlin.collections.component2
 
 /**
  * Represents a generic text to be evaluated with a [Context] in the UI layer.
@@ -63,6 +68,58 @@ data class PhraseModel(
     @Composable
     override fun localized(): String {
         val string = stringResource(this.resource)
+        if (args.isEmpty()) {
+            return string
+        }
+
+        var resultString = string
+        args.forEach { (key, textData) ->
+            val placeholder = "{$key}"
+            val value = textData.localized()
+            resultString = resultString.replace(placeholder, value)
+        }
+
+        return resultString
+    }
+}
+
+/**
+ * A text data model that represents a plural string resource.
+ *
+ * Usage example:
+ *
+ * <plurals name="new_message">
+ *  <item quantity="one">{quantity} new message from {name}</item>
+ *  <item quantity="other">{quantity} new messages from {name}</item>
+ * </plurals>
+ *
+ * ```kotlin
+ * PluralPhraseModel(
+ *    resource = Res.plurals.item_count,
+ *    quantity = itemCount,
+ *    "quantity" to FixedString(itemCount.toString())
+ *    "name" to FixedString("John")
+ * )
+ * ```
+ */
+data class PluralResourceString(
+    val resource: PluralStringResource,
+    val quantity: Int,
+    val args: Map<String, TextData> = emptyMap(),
+) : TextData() {
+    constructor(
+        resource: PluralStringResource,
+        quantity: Int,
+        vararg args: Pair<String, TextData>,
+    ) : this(
+        resource = resource,
+        quantity = quantity,
+        args = args.toMap(),
+    )
+
+    @Composable
+    override fun localized(): String {
+        val string = pluralStringResource(this.resource, quantity)
         if (args.isEmpty()) {
             return string
         }
